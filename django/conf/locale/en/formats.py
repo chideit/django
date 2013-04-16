5981447526942d2b88b59effda1f071a27bf0e84
Samuel Cormier-Iijima <sam@chide.it>
2013-04-16 16:00:36 +0000

    Better short date format for english

--- django/conf/locale/en/formats.py
+++ django/conf/locale/en/formats.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 
 # The *_FORMAT strings use the Django date format syntax,
 # see http://docs.djangoproject.com/en/dev/ref/templates/builtins/#date
-DATE_FORMAT = 'N j, Y'
+DATE_FORMAT = 'M j, Y'
 TIME_FORMAT = 'P'
 DATETIME_FORMAT = 'N j, Y, P'
 YEAR_MONTH_FORMAT = 'F Y'
